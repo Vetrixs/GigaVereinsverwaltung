4d3bff0173acea42e56efcc0583c3b9a884a0db7
masouri <dregion77@gmail.com>
2023-01-26 14:17:57 +0100

    migration member

---
+++ app/database/migrations/2023_01_26_130612_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone_number');
+            $table->string('email_address');
+            $table->boolean('is_employed');
+            $table->date('birthday');
+            $table->string('iban');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
