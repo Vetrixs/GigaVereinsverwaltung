b0332f5a380d51766ae02a4bb02a858b62127e33
masouri <dregion77@gmail.com>
2023-02-09 13:34:34 +0100

    fixxed vuetify

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -1,4 +1,8 @@
 const mix = require('laravel-mix');
+const path = require('path')
+require('dotenv').config();
+require('vuetifyjs-mix-extension')
+require('laravel-mix-eslint-config');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,7 +14,51 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+const proxy_url = process.env.APP_URL;
 
+const options = {
+    extract: 'css/vuetify-components.css'
+}
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+.vuetify('vuetify-loader', options)
+.vue()
+.alias({
+    vue$: path.join(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js'),
+    '@': path.join(__dirname, 'resources/js')
+})
+.eslint({
+    enforce: 'pre',
+    test: /\.(js|vue)$/,
+    loader: 'eslint-loader',
+    exclude: /(node_modules)/,
+    options: {
+      configFile: 'quality/.eslintrc.js',
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      failOnWarning: true,
+      fix: true,
+    }
+  })
+.extract()
+.sourceMaps();
+
+// mix.js('resources/js/app.js', 'public/js').vue()
+//     .postCss('resources/css/app.css', 'public/css', [
+//         require('postcss-import'),
+//         require('tailwindcss'),
+//     ])
+//     .webpackConfig(require('./webpack.config'));
+
+if (mix.inProduction()) {
+    mix.version();
+} else {
+    mix.browserSync({
+        // url for WebpackDevServer
+        proxy: {
+            target: proxy_url
+        },
+        injectChanges: true,
+    });
+}
+
