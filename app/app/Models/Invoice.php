a8e27b3c9b07f7159068281e489f37a4a78e1bcb
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2023-02-09 12:55:25 +0100

    fillables für Model

--- app/app/Models/Invoice.php
+++ app/app/Models/Invoice.php
@@ -11,6 +11,12 @@ class Invoice extends Model
     use HasFactory;
     use SoftDeletes;
 
+    protected $fillable = [
+        'price',
+        'description',
+        'invoice_type'
+    ];
+
     public function member(){
         return $this->belongsTo(Member::class);
     }
